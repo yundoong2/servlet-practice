b512cbec3d75a38d23d1324040c0b909160b16f3
YHCHOI <cyh6809@gmail.com>
2023-04-19 00:42:39 +0900

    서블릿, JSP에 MVC 패턴 적용 실습

---
+++ src/main/java/com/hello/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,35 @@
+package com.hello.servlet.web.servletmvc;
+
+import com.hello.servlet.domain.member.Member;
+import com.hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 회원 목록 조회 컨트롤러
+ */
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MvcMemberListServlet.service");
+        List<Member> members = memberRepository.findAll();
+
+        //Model에 저장
+        request.setAttribute("members", members);
+        
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
